0d3e774262ed08aae712347e40b42c870f55eb93
wxlfe <wxlfe@protonmail.com>
2019-05-24 20:47:57 -0400

    Better Mobile Compatibility

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ display: 'flex', flexDirection:'row', justifyContent: 'space-around', textAlign: 'center', height: '45rem', }}>
+    <div style={{ display: 'flex', flexDirection:'row', justifyContent: 'space-around', textAlign: 'center', height: '45rem', maxWidth: '90vw' }}>
       <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'center', }}>
-        <h1 style={{ fontSize: '10rem', fontWeight: '200', }}>wxlfe</h1>
+        <h1 style={{ fontSize: '20vw', fontWeight: '200', }}>wxlfe</h1>
       </div>
       <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'center', }}>
-        <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', }}>
-          <a href='#' style={{ textDecoration: 'none', color: 'white', margin: '.5em' }}>github</a>
-          <a href='#' style={{ textDecoration: 'none', color: 'white', margin: '.5em' }}>linkedin</a>
-          <a href='#' style={{ textDecoration: 'none', color: 'white', margin: '.5em' }}>email</a>
-        </div>
+          <a href='#' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>github</a>
+          <a href='#' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>linkedin</a>
+          <a href='#' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>email</a>
       </div>
     </div>
   </Layout>
