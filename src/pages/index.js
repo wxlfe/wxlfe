349cb5469263b56d9b183b5e3b214956e85997de
Nate Wolfe <nate@wxlfe.dev>
2020-08-03 16:48:59 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ display: 'flex', flexDirection:'row', justifyContent: 'space-around', textAlign: 'center', height: '50vh', maxWidth: '90vw' }}>
+    <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'space-around', textAlign: 'center', height: '50vh', maxWidth: '90vw' }}>
       <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'center', }}>
-        <h1 style={{ fontSize: '20vw', fontWeight: '200', }}>wxlfe</h1>
+        <h1 style={{ fontSize: '20vw', fontWeight: '200', }}>Nate Wolfe</h1>
       </div>
-      <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'center', }}>
+      <div style={{ display: 'flex', flexDirection:'row', justifyContent: 'center', }}>
           <a href='https://github.com/wxlfe' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>github</a>
           <a href='https://linkedin.com/in/wxlfe' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>linkedin</a>
           <a href='mailto:nate@wxlfe.dev' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>email</a>
