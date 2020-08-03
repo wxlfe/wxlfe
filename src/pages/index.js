dbff1f56a5634a240d8832a58b4208a8c65af1e9
Nate Wolfe <nate@wxlfe.dev>
2020-08-03 16:57:25 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,12 @@ const IndexPage = () => (
       <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'center', }}>
         <h1 style={{ fontWeight: '200' }}>Nate Wolfe</h1>
         <h3 style={{ fontweight: '200' }}>Student, Tutor, and Software Engineer</h3>
+        <p>
+            Hi, I'm Nate. I recently earned my Associate's Degree in Computer Science from Dalton State, and I'm pursuing my Bachelor's Degree in Mathematics. I'm primarily interested in Web 3 and the accompanying technologies built on blockchains such as Ethereum, as these new technologies could do wonders for user privacy and security. I'm experienced in front-end web development and design, frameworks such as React, Angular, and JQuery, and languages such as Java, C#, SQL, and Python. 
+        </p>
+        <p>
+            If you'd like to save this site for later, try pinning it to your home screen - it's a progressive web app, so it'll launch as an app!
+         </p>
       </div>
       <div style={{ display: 'flex', flexDirection:'row', justifyContent: 'center', }}>
           <a href='https://github.com/wxlfe' style={{ textDecoration: 'none', color: 'white', margin: '.5em', fontSize: '4vw', }}>github</a>
