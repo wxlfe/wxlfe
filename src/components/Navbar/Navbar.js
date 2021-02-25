be98e9d1416ba26516c8a2ad9fba5edb36bbc5d3
Nate Wolfe <nate@wxlfe.dev>
2021-02-24 22:36:56 -0500

    Initial Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
